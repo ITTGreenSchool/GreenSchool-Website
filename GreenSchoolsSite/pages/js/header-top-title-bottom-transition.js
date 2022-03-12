6e117a6398dd74c768a83a086e6bbe08d016750f
Alessandro Ghironi <ghironialessandro27@gmail.com>
2022-03-13 00:53:36 +0100

    Site Updated 13/03/2022

--- GreenSchoolsSite/pages/js/header-top-title-bottom-transition.js
+++
@@ -1,3 +0,0 @@
-$(document).on('scroll', function(){
-    $('.header-title-bottom').css("left", Math.max(0 - 1.5*window.scrollY, -1000) + "px");
-})
--- GreenSchoolsSite/pages/js/header-top-title-top-transition.js
+++
@@ -1,3 +0,0 @@
-$(document).on('scroll', function(){
-    $('.header-title-top').css("left", Math.max(0 + 1.5*window.scrollY, 0) + "px");
-})
